10bd8b5fd8acfeec2f01c6c89bd31a1359426ad2
Morgan Aubert <morgan@aubert.email>
2019-03-23 18:24:18 -0400

    Add basic behaviour for responsive navbar content

--- notabanane/static/js/App.js
+++ notabanane/static/js/App.js
@@ -47,4 +47,33 @@ document.addEventListener('DOMContentLoaded', () => {
       navBarSearchInput.value = '';
     }
   });
+
+  // Initializes responsive-specific behaviours.
+  const largeDevicesWidth = 1025;
+  const navBarMenu = document.getElementById('notabanane_navbar');
+  const navBarToggler = document.querySelector('.navbar-burger');
+
+  function toggleAction(ev) {
+    ev.stopImmediatePropagation();
+    navBarMenu.classList.toggle('opened');
+    document.body.classList.toggle('navbar-menu-opened');
+    navBarToggler.classList.toggle('is-active');
+  }
+
+  function closeNavBar(ev) {
+    ev.stopImmediatePropagation();
+    if (!ev.target.closest('.navbar-toggler, #notabanane_navbar')) {
+      navBarMenu.classList.remove('opened');
+      document.body.classList.remove('navbar-menu-opened');
+      navBarToggler.classList.remove('is-active');
+    }
+  }
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth <= largeDevicesWidth) {
+      navBarToggler.addEventListener('click', ev => toggleAction(ev));
+      document.addEventListener('click', ev => closeNavBar(ev));
+    }
+  });
+  window.dispatchEvent(new Event('resize'));
 });
