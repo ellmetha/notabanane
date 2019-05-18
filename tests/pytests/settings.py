e9a71b04a66c09d77550c5fd064eece7228f280b
Morgan Aubert <morgan@aubert.email>
2019-05-18 14:46:18 -0400

    Fix test environment configuration

--- tests/pytests/settings.py
+++ tests/pytests/settings.py
@@ -2,7 +2,6 @@ import os
 
 
 TEST_ROOT = os.path.abspath(os.path.dirname(__file__))
-os.environ['ENVSETTINGS_FILEPATH'] = os.path.join(TEST_ROOT, '.env.json')
 
 
 # PROJECT CONFIGURATION
@@ -17,3 +16,6 @@ from notabanane_project.settings.base import *  # noqa, isort:skip
 MEDIA_ROOT = os.path.join(TEST_ROOT, '_testdata/media/')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 TEMPLATES[0]['DIRS'] += [os.path.join(TEST_ROOT, '_testdata/templates/'), ]  # noqa: F405
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+DEFAULT_FROM_EMAIL = 'test@example.com'
+PROJECT_CONTACT_EMAIL = 'test@example.com'
