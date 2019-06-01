bc6cbb536f378494f64be56ba45756decb1b7bd7
Morgan Aubert <morgan@aubert.email>
2019-05-31 22:03:33 -1000

    Add missing unit tests for iso8601 template filter

--- tests/pytests/unit/common/templatetags/test_common_duration_tags.py
+++ tests/pytests/unit/common/templatetags/test_common_duration_tags.py
@@ -1,8 +1,13 @@
 import datetime as dt
 
-from main.common.templatetags.common_duration_tags import duration
+from main.common.templatetags.common_duration_tags import duration, iso8601
 
 
 def test_duration_filter_works_as_expected():
     assert duration(dt.timedelta(minutes=40)) == '40 minutes'
     assert duration(dt.timedelta(hours=1)) == '1 heure'
+
+
+def test_iso8601_filter_works_as_expected():
+    assert iso8601(dt.timedelta(minutes=40)) == 'PT40M'
+    assert iso8601(dt.timedelta(hours=1)) == 'PT1H'
