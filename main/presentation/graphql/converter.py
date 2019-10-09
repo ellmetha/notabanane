914e403f24d7174e78094b4da9ba6a0bb843264e
Morgan Aubert <morgan@aubert.email>
2019-10-08 22:07:04 -0400

    Rename GraphQL converter

--- main/presentation/graphql/converter.py
+++ main/presentation/graphql/converter.py
@@ -13,5 +13,5 @@ from graphene_django.forms.converter import convert_form_field
 
 
 @convert_form_field.register(forms.MultipleChoiceField)
-def convert_form_field_to_string_list(field):
+def convert_form_muliple_choice_field_to_string_list(field):
     return graphene.List(graphene.String, description=field.help_text, required=field.required)
