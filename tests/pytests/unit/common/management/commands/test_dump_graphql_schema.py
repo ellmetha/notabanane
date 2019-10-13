39fce7eef1f4dd31ff1e85fc52375f5f11403d11
Morgan Aubert <morgan@aubert.email>
2019-10-12 22:11:50 -0400

    Add tests for the dump_graphql_schema management command

---
+++ tests/pytests/unit/common/management/commands/test_dump_graphql_schema.py
@@ -0,0 +1,32 @@
+import tempfile
+from io import StringIO
+
+import pytest
+from django.core.management import call_command
+from django.utils.translation import activate, get_language
+from graphene_django.settings import graphene_settings
+from graphql.utils import schema_printer
+
+
+class TestDumpGraphqlSchema:
+    @pytest.fixture(autouse=True)
+    def setup_and_teardown(self):
+        current_locale = get_language()
+        activate('fr')
+        self.stdout = StringIO()
+        self.stderr = StringIO()
+        yield
+        self.stdout.close()
+        self.stderr.close()
+        activate(current_locale)
+
+    def test_can_export_the_dump_of_the_graphql_schema_to_the_standard_output(self):
+        call_command('dump_graphql_schema', stdout=self.stdout)
+        self.stdout.seek(0)
+        assert self.stdout.read() == schema_printer.print_schema(graphene_settings.SCHEMA)
+
+    def test_can_export_the_dump_of_the_graphql_schema_to_a_single_file(self):
+        export_filename = tempfile.mktemp()
+        call_command('dump_graphql_schema', output=export_filename, stdout=self.stdout)
+        with open(export_filename) as fd:
+            assert fd.read() == schema_printer.print_schema(graphene_settings.SCHEMA)
