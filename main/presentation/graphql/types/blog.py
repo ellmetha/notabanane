fcf2fe879b73720e37f50a78009837b061fcf55f
Morgan Aubert <morgan@aubert.email>
2019-10-27 22:02:26 -0400

    Ensure RecipePage type only works with live entries

--- main/presentation/graphql/types/blog.py
+++ main/presentation/graphql/types/blog.py
@@ -28,6 +28,11 @@ class RecipePageType(DjangoObjectType):
         fields = ('title', 'date', 'header_image_url', 'url', 'dish_types', )
         filter_fields = ('dish_types', )
 
+    @classmethod
+    def get_queryset(cls, queryset, info):
+        """ Returns the default queryset to use for the RecipePage type. """
+        return RecipePage.objects.select_related('header_image').live().order_by('-date')
+
     def resolve_header_image_thumbnail(self, info):
         """ Returns the thumbnail URL of the header image. """
         return self.header_image.get_rendition('fill-555x312').url
