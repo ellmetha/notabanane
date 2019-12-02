5277bed97d70f8ec56beab4bdaab3887c0089be9
Morgan Aubert <morgan@aubert.email>
2019-12-01 19:22:19 -0500

    Fix GraphQL client URL

--- main/static/js/controllers/RecipeSearchController/RecipeSearchController.jsx
+++ main/static/js/controllers/RecipeSearchController/RecipeSearchController.jsx
@@ -11,7 +11,7 @@ import Search from './components/Search';
 
 
 const client = new ApolloClient({
-  uri: 'http://127.0.0.1:8000/graphql/',
+  uri: '/graphql/',
   headers: {
     'X-CSRFToken': getCSRFToken(),
   },
