4946b1123ac9429bb7d9cb72612ca0aa47c637ab
Morgan Aubert <morgan@aubert.email>
2019-12-31 12:09:12 -0500

    Add test for ResultListItem component

---
+++ tests/jstests/controllers/RecipeSearchController/components/ResultListItem-test.jsx
@@ -0,0 +1,31 @@
+/* eslint comma-dangle: 0 */
+/* eslint import/extensions: [0, {}] */
+/* eslint import/no-unresolved: [0, {}] */
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+
+import ResultListItem from 'controllers/RecipeSearchController/components/ResultListItem';
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+describe('<ResultListItem />', () => {
+  test('can be properly rendered', () => {
+    const recipe = {
+      title: 'Cold soup',
+      url: 'https://example.com/cold-soup',
+      headerImageThumbnail: 'https://example.com/static-images/cold-soup.jpg',
+      formattedDate: 'Dec. 27 2019',
+    };
+
+    const component = Enzyme.shallow(
+      <ResultListItem
+        recipe={recipe}
+      />
+    );
+    expect(component.find('.post-title').text()).toEqual(recipe.title);
+  });
+});
---
+++ tests/jstests/wrapper.js
@@ -0,0 +1,12 @@
+module.exports = require('babel-jest').createTransformer({
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react"
+  ],
+  plugins: [
+    [
+      "@babel/plugin-transform-runtime",
+      { "regenerator": true }
+    ]
+  ]
+});
