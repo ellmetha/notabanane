1a9926802b75bec3314fec13040f80d4419c13bf
Morgan Aubert <morgan@aubert.email>
2020-01-02 21:50:03 -0500

    Add missing tests for the FilterForm component

--- tests/jstests/controllers/RecipeSearchController/components/FilterForm-test.jsx
+++ tests/jstests/controllers/RecipeSearchController/components/FilterForm-test.jsx
@@ -12,6 +12,7 @@ import FilterForm from 'controllers/RecipeSearchController/components/FilterForm
 
 Enzyme.configure({ adapter: new Adapter() });
 
+
 const waitForComponentToPaint = async (wrapper) => {
   await act(async () => {
     await new Promise(resolve => setTimeout(resolve, 0));
@@ -33,17 +34,53 @@ describe('<FilterForm />', () => {
     expect(component.find('#recipe_filters_form').length).toEqual(1);
   });
 
-  test('calls the submit callback when the form is submitted', (done) => {
+  test('properly updates dish type checkboxes when values change', (done) => {
     const onSubmitFilters = (values) => {
-      expect(values).toEqual({ dishTypes: ["APPETIZERS"], seasons: [] });
+      expect(values.dishTypes).toEqual(["APPETIZERS"]);
       done();
     };
     const component = Enzyme.mount(
       <FilterForm
-        onSubmitFilters={onSubmitFilters}
+        onSubmitFilters={(values) => {
+          expect(values.dishTypes).toEqual(["APPETIZERS"]);
+          done();
+        }}
       />
     );
     waitForComponentToPaint(component);
     component.find('input[value="APPETIZERS"]').simulate('change', { target: { checked: true } });
+
+    component.setProps({
+      onSubmitFilters: (values) => {
+        expect(values.dishTypes).toEqual([]);
+        done();
+      },
+    });
+    component.update();
+
+    component.find('input[value="APPETIZERS"]').simulate('change', { target: { checked: false } });
+  });
+
+  test('properly updates season checkboxes when values change', (done) => {
+    const component= Enzyme.mount(
+      <FilterForm
+        onSubmitFilters={(values) => {
+          expect(values.seasons).toEqual(['WINTER']);
+          done();
+        }}
+      />
+    );
+    waitForComponentToPaint(component);
+    component.find('input[value="WINTER"]').simulate('change', { target: { checked: true } });
+
+    component.setProps({
+      onSubmitFilters: (values) => {
+        expect(values.seasons).toEqual([]);
+        done();
+      },
+    });
+    component.update();
+
+    component.find('input[value="WINTER"]').simulate('change', { target: { checked: false } });
   });
 });
