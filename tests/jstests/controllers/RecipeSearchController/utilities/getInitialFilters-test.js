02e94aefee84681817ff570674cf523dd609f4c4
Morgan Aubert <morgan@aubert.email>
2020-01-04 21:35:07 -0500

    Add tests for the getInitialFilters utility

---
+++ tests/jstests/controllers/RecipeSearchController/utilities/getInitialFilters-test.js
@@ -0,0 +1,28 @@
+/* eslint comma-dangle: 0 */
+/* eslint import/extensions: [0, {}] */
+/* eslint import/no-unresolved: [0, {}] */
+
+import { createBrowserHistory } from 'history';
+import queryString from 'query-string';
+
+import getInitialFilters from 'controllers/RecipeSearchController/utilities/getInitialFilters';
+import history from 'core/history';
+
+
+describe('getInitialFilters()', () => {
+  test('extracts dish types values from the history', () => {
+    history.push({
+      location: '/',
+      search: queryString.stringify({ dishTypes: ['APPETIZERS', 'SOUPS'] }),
+    });
+    expect(getInitialFilters()).toEqual({ dishTypes: ['APPETIZERS', 'SOUPS'], seasons: [] });
+  });
+
+  test('extracts seaons values from the history', () => {
+    history.push({
+      location: '/',
+      search: queryString.stringify({ seasons: ['WINTER', 'SPRING'] }),
+    });
+    expect(getInitialFilters()).toEqual({ dishTypes: [], seasons: ['WINTER', 'SPRING'] });
+  });
+});
