96933889d0931286f26df6f9e28fdcee485c5b3c
Morgan Aubert <morgan@aubert.email>
2020-01-11 12:48:04 -0500

    Add unit tests for getCSRFToken js util

---
+++ tests/jstests/core/getCSRFToken-test.js
@@ -0,0 +1,15 @@
+/* eslint import/extensions: [0, {}] */
+/* eslint import/no-unresolved: [0, {}] */
+
+import Cookies from 'js-cookie';
+
+import getCSRFToken from 'core/getCSRFToken';
+
+
+describe('getCSRFToken', () => {
+  test('returns the CSRF token from the corresponding cookie', () => {
+    const spy = jest.spyOn(Cookies, 'get');
+    getCSRFToken();
+    expect(spy).toHaveBeenCalledWith('csrftoken');
+  });
+});
