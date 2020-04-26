58b9aec79ae3f3da3cf3fb0c868a14e7c27e8024
Morgan Aubert <morgan@aubert.email>
2020-04-26 12:41:58 -0400

    Add comment test factory

---
+++ main/apps/comment/test/factories.py
@@ -0,0 +1,28 @@
+"""
+    Comment model factories
+    =======================
+
+    This module defines model factories that can be used to create model instances when preparing
+    test data.
+
+"""
+
+import factory
+from faker import Factory
+
+from ..models import Comment
+
+
+fake = Factory.create()
+
+
+class CommentFactory(factory.DjangoModelFactory):
+    """ Factory class for the ``Comment`` model. """
+
+    unregistered_author_email = factory.LazyFunction(fake.email)
+    unregistered_author_name = factory.LazyFunction(fake.first_name)
+
+    content = factory.LazyAttribute(lambda o: fake.sentences(nb=3, ext_word_list=None))
+
+    class Meta:
+        model = Comment
